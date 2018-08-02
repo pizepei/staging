840eb3b071e5a37162f217905bc5047f305d2093
皮泽培 <848733913@qq.com>
2018-08-02 18:01:47 +0800

    初始化提交

---
+++ src/Controller.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pizepei
+ * Date: 2018/8/2
+ * Time: 17:48
+ * @title 控制器基类
+ */
+namespace pizepei\staging;
+
+class Controller
+{
+    /**
+     * 初始化
+     */
+
+
+    /**
+     * 模板渲染、输出json等等
+     */
+
+    /**
+     * 权限控制
+     */
+
+
+    
+
+
+
+}
---
+++ src/Route.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pizepei
+ * Date: 2018/8/2
+ * Time: 16:06
+ * @title 路由
+ */
+
+namespace pizepei\staging;
+
+
+class Route
+{
+
+}
