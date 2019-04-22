9fb9d3dd2c0e8f54d36a22d29e2b33c543ca7cd0
皮泽培 <848733913@qq.com>
2019-04-22 18:06:36 +0800

    权限支持

--- src/Authority.php
+++ src/Authority.php
@@ -73,8 +73,7 @@ class Authority
         // *方法路由：注册到不同操作权限资源里面用authGroup【admin.bbx:user.bbx】中文名字、注册扩展扩展authExtend  控制器：方法（方法里面有返回数据、）
         $AccountService = new AccountService();
         $Redis = Redis::init();
-        var_dump($_SERVER);
-        return $AccountService->decodeLogonJwt($this->pattern,$_SERVER['HTTP_ACCESS_TOLEN'],$Redis);
+        return $AccountService->decodeLogonJwt($this->pattern,$_SERVER['HTTP_ACCESS_TOKEN'],$Redis);
 
     }
 }
