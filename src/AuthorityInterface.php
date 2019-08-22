b86b8ef94f47e8058103e5c6dfdffcdcd0cf04d0
皮泽培 <848733913@qq.com>
2019-08-22 17:56:06 +0800

    重新规划bases

---
+++ src/AuthorityInterface.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pizepei
+ * Date: 2019/1/15
+ * Time: 16:24
+ * @title 权限控制基础类 
+ */
+declare(strict_types=1);
+
+namespace pizepei\staging;
+
+interface  AuthorityInterface
+{
+//    /**
+//     * Authority constructor.
+//     * @param $pattern
+//     * @param App $app
+//     */
+//    public function __construct($pattern,App $app);
+//    /**
+//     * @Author 皮泽培
+//     * @Created 2019/8/22 14:21
+//     * @param $name
+//     * @title  路由标题
+//     * @return mixed
+//     * @throws \Exception
+//     */
+//    public function __get($name);
+//    /**
+//     * 判断是否登录
+//     * @throws \Exception
+//     */
+//    public function isLogin();
+//    /**
+//     * 判断是有权限
+//     * @throws \Exception
+//     */
+//    public function grantCheck();
+
+}
